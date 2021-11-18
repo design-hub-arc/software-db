350e904c6098fa47fc93546edbdc65752d7ac896
Matt Crow <greengrappler12@gmail.com>
2021-11-18 13:06:00 -0800

    started work on cookies, need database

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 Dependencies
 */
 const express = require("express");
+const session = require("express-session");
 
 
 
@@ -12,8 +13,32 @@ const PORT = 3000; // TODO: read from config file
 
 const app = express();
 app.use(express.static("public"));
+app.use(session({
+    secret: "get a better secret!", // TODO: read from config file
+    resave: false, // only save when cookies change
+    saveUninitialized: false, // don't save cookies with no data
+    cookie: {
+        path: "/",
+        secure: !true, // TODO: needs to be false for localhost, true for production
+        maxAge: 1000 * 60 * 60 // keep cookie for 1 hour
+    }
+}));
+
+
+
 app.get("/", (req, res)=>{
-    res.send("This works!");
+    if(!req.session.count){
+        req.session.count = 0;
+    }
+    ++req.session.count;
+    res.send(`This works! ${req.session.count}`);
+});
+
+app.get("/logout", (req, res)=>{
+   req.session.destroy((error)=>{
+       console.error("Failed to destroy session");
+       console.error(error);
+   });
 });
 
 
